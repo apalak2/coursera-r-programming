d66c0e336df5783ab779481d373771cb5b29516f
apalak2 <apalak2@uic.edu>
2016-05-15 17:29:37 -0500

    Add files via upload

---
+++ pollutantMean.R
@@ -0,0 +1,15 @@
+pollutantmean<-function(directory, pollutant, id){ 
+  #setting the working directory to the one containing required .csv files
+  specdatapath<-paste("C:/Users/aniru_000/Desktop/JHU R Course",directory,sep = "/")
+  setwd(specdatapath)
+  #Creating a combined data frame including all the files
+  files<-list.files(pattern = "\\.csv")
+  tables <- lapply(files, read.csv, header = TRUE)
+  combineddata <- do.call(rbind , tables)
+  #Creating a subset according to the function arguments i.e. the ID
+  dataatstake<-subset(combineddata,combineddata$ID %in% id)
+  #Calculating mean value of the required pollutant
+  pollutantmeanvalue<- mean(dataatstake[,pollutant],na.rm = TRUE)
+  #Printing the pollutant mean value
+  print(pollutantmeanvalue)
+}
